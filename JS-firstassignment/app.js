bbaf7f706449a67384c3373ba6e4973779c666c6
aizafatima17 <aizafatiam1722@gmail.com>
2024-04-24 12:55:59 -0700

    Doing first js assignment using Alert

---
+++ JS-firstassignment/app.js
@@ -0,0 +1,5 @@
+alert("Thanks For Visiting My Website!")
+//alert("Error! Please Enter a Valid Password.")
+//alert("error!\n Please Enter Valid CNIC.")
+//alert("Welcome To JS")
+//alert("Happy Coding")
